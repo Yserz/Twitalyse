17f16551a27227c919584d05d4d7f37bd8cff6e5
Kaain <ott@fh-brandenburg.de>
2012-12-05 01:40:46 +0100

    update coords

--- src/main/java/de/fhb/twitalyse/CoordsInLangTopology.java
+++ src/main/java/de/fhb/twitalyse/CoordsInLangTopology.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 Michael Koppen
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package de.fhb.twitalyse;
 
 import java.io.IOException;
@@ -158,7 +142,7 @@ public class CoordsInLangTopology {
 	}
 
 	public static void main(String[] args) throws IOException {
-		AlphaTwitalyseTopology a = new AlphaTwitalyseTopology();
+		CoordsInLangTopology a = new CoordsInLangTopology();
 		try {
 			a.startTopology(args);
 		} catch (AlreadyAliveException e) {
